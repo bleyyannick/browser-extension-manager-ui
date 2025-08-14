202a0fdde42d23acf49dde201cdae8f9f883cac8
Yannick Bley <bleyyannickpro@gmail.com>
2025-08-14 13:51:30 +0200

    fix: fixing config for css files

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base:'/browser-extension-manager-ui/',
+  base: '/browser-extension-manager-ui/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
+    css: true,
+    alias: {
+      '@': './src',
+    },
     include: [
       '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
       '**/__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
     ]
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+    }
   }
 })
