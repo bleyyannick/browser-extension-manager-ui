9d608195ac4e2824268717bc804aa0d77151992f
Yannick Bley <bleyyannickpro@gmail.com>
2025-08-14 11:47:47 +0200

    add vitest in project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:'/browser-extension-manager-ui/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    include: [
+      '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+      '**/__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
+    ]
+  }
 })
