88b7c65f08927050315665fa81a4aa381686bb79
Yannick Bley <bleyyannickpro@gmail.com>
2025-08-29 10:02:12 +0200

    chore: enhancing performance with avoiding useless rerendering

--- src/hooks/useExtensionsManager.tsx
+++ src/hooks/useExtensionsManager.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import { ExtensionFilter, type Extension, type ExtensionFilterType  } from "../types";
 
@@ -8,23 +8,25 @@ export const useExtensionsManager = (
 
    const [extensions, setExtensions] = useState<Extension[]>(intialExtensions);
 
-   const handleRemoveExtension = (extensionName: string) => {
-     setExtensions((prev) => [...prev].filter((ext) => ext.name !== extensionName));
-   };
+   const handleRemoveExtension = useCallback((extensionName: string) => {
+     setExtensions((prev) => prev.filter((ext) => ext.name !== extensionName));
+   }, []);
 
-   const toggleExtension = (name: string) => {
-    setExtensions((prev) =>
-      prev.map((ext) =>
-        ext.name === name? { ...ext, isActive: !ext.isActive } : ext
-      )
-    );
-  };
+   const toggleExtension = useCallback((name: string) => {
+      setExtensions((prev) =>
+        prev.map((ext) =>
+          ext.name === name? { ...ext, isActive: !ext.isActive } : ext
+        )
+      );
+  }, []);
 
-  const filteredExtensionList = extensions.filter((ext) => {
-          if (filter === ExtensionFilter.ALL) return true;
-          if (filter === ExtensionFilter.ACTIVE) return ext.isActive;
-          return !ext.isActive;
-      });
+  const filteredExtensionList = useMemo(() => {
+    return extensions.filter((ext) => {
+      if (filter === ExtensionFilter.ALL) return true;
+      if (filter === ExtensionFilter.ACTIVE) return ext.isActive;
+      return !ext.isActive;
+    });
+  }, [extensions, filter]);
   
   return {
         handleRemoveExtension,
